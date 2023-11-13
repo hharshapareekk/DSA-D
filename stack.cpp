641670e8f75952a88b4e1354c013c83327e6ee25
Harsha Pareek <harshapareek14@gmail.com>
2023-11-14 00:42:45 +0530

    stacks

---
+++ stack.cpp
@@ -0,0 +1,138 @@
+
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// stack using STL
+/*int main()
+{
+    // creation of stack
+    stack<int> s;
+    // push operation
+    s.push(4);
+    s.push(8);
+
+    // pop operation
+    s.pop();
+
+    // top of the stack
+    cout << "top element is:" << s.top() << endl;
+
+    if (s.empty())
+    {
+        cout << "Stack is empty";
+    }
+    else
+    {
+        cout<<"Stack is not empty";
+    }
+
+    return 0;
+}*/
+
+// stack using implementation
+//1.ARRAY
+/*class Stack
+{
+    // properties
+public:
+    int *arr;
+    int top;
+    int size;
+
+    // behaviour
+    Stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    // push
+    void push(int element)
+    {
+        if (size - top > 1) // let's say size=0 no element (size-top==1 hence space he nhi hy) or could use top<size
+        {
+            top += 1;
+            arr[top] = element;
+            cout<<"Element "<<element<<" is inserted"<<endl;
+        }
+        else
+        {
+            cout << "Stack overflow" << endl;
+        }
+    }
+
+    // pop
+    void pop()
+    {
+        if (top >= 0)
+        {
+            top--;
+            cout<<"Element "<<arr[top+1]<<" is popped"<<endl;
+        }
+        else
+        {
+            cout << "Stack underflow";
+        }
+    }
+
+    // top
+    int peek()
+    {
+        if (top >= 0)
+            return arr[top];
+        else
+        {
+            cout << "Stack is empty" << endl;
+        }
+    }
+
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    Stack st(5);
+    st.push(5);
+    st.push(14);
+    st.push(12);
+    st.push(14);
+    st.push(22);
+    st.push(222);
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+}*/
+
+
+//2.Linkedlist (pending)
+
+class Stack{
+    public:
+        int data;
+        Stack *next;
+        Stack *prev;
+
+    Stack(int d)
+    {
+        this->data=d;
+        this->prev=NULL;
+        this->next=NULL;
+    }
+
+};
